b79d0f4d4f6acd7d67f9ea18a08fdabaf945bdfc
nicholas-auyeung <nicholas.auyeung@hcl.com>
2020-12-02 13:30:52 -0800

    prototype

--- EmailIdValidation.java
+++ EmailIdValidation.java
@@ -14,7 +14,7 @@ public class EmailIdValidation {
 	public static void main(String[] args) {
 		
 		boolean stillRunning, finished = true;
-		String emailInput;
+		String scannerInput;
 		
 		Scanner scan = new Scanner(System.in);
 		
@@ -23,26 +23,41 @@ public class EmailIdValidation {
 		
 		Pattern pattern = Pattern.compile(regex);
 		
+		System.out.println("Welcome to Email Validation" + "\n");
 		
 		while(stillRunning = true) {
-			System.out.println("Welcome to Email Validation" + "\n");
 			System.out.println("Please enter the emails to validate'");
-			System.out.println("Type 'F' when done");
+			System.out.println("Enter 'F' when done");
 			while(finished = true) {
-				emailInput = scan.next();
-				if(emailInput.compareTo("F") == 0) {
+				scannerInput = scan.next();
+				if(scannerInput.compareTo("F") == 0) {
 					break;
 				}
 				emailList.add(scan.next());
 			}
 			for(String email : emailList) {
 				Matcher matcher = pattern.matcher(email);
+				System.out.println("Results:" + "\n");
 				System.out.println("E-mail: " + email + "is" + (matcher.matches() ? "valid":"invalid"));
+				emailMap.put(email, matcher.matches());
 			}
 			
-			
-			
-			scan.close();
+			System.out.println("Type 'log' to view the emails attempted...");
+			System.out.println("Enter any key to continue or type 'exit' to exit");
+			scannerInput = scan.next();
+			if(scannerInput.compareTo("log") == 0) {
+				int numEntry = 0;
+				for(HashMap.Entry<String, Boolean> entry : emailMap.entrySet()) {
+					numEntry += 1;
+					String email = entry.getKey();
+					Boolean valid = entry.getValue();
+					System.out.println(numEntry + ": " + "Email: " + email + "Valid: " + valid );
+				}
+			}else if(scannerInput.compareTo("exit") == 0) {
+				break;
+			}
 		}
+		
+		scan.close();
 	}
 }
